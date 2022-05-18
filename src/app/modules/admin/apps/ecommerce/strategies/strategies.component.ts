e2ef9c83feee5f4ebbda29a6287c8eef6113d60b
manasvivaghasiya <96326072+manasvivaghasiya@users.noreply.github.com>
2022-05-18 18:46:54 +0530

    ...

--- src/app/modules/admin/apps/ecommerce/strategies/strategies.component.ts
+++ src/app/modules/admin/apps/ecommerce/strategies/strategies.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatCheckboxChange } from '@angular/material/checkbox';
 
@@ -9,97 +9,95 @@ import { MatCheckboxChange } from '@angular/material/checkbox';
 })
 export class StrategiesComponent implements OnInit {
 
-  toppings:FormGroup;
-  btnDisabled:false;
+  toppings: FormGroup;
+  btnDisabled: false;
   filter = false;
-  options:any=[];
-  check:any=[];
-  checked:any=[];
-  rightArray:any=[];
-  rightcheck:any=[];
-  name:boolean;
+  options: EnumValue[] = [];
+  check: any = [];
+  checked: any = [];
+  rightArray: any = [];
+  rightcheck: any = [];
+  name: boolean;
   selected: boolean = false;
-  selectCheck:boolean = false;
+  selectCheck: boolean = false;
+  rightCheckArray: EnumValue[] = [];
+  leftCheckArray: EnumValue[] = [];
+  tempCheckArray: EnumValue[] = [];
 
-  constructor(private fb: FormBuilder) { 
-    // this.toppings = fb.group({
-    //   MinCostOfPrinting: [''],
-    //   MinShippingTime: [''],
-    //   VendorPerformance: [''],
-    //   IFCVendor: [''],
-
-    //   VendorCapacity: [''],
-    //   Ranking: [''],
-    //   RiskFactor: [''],
-    //   DeactivationDate: [''],
-
-    // });
-
-    this.options = [
-      {name:'Min Cost Of Printing',selected:''},
-      {name:'Min Shipping Time',selected:''},
-      {name:'Vendor Performance',selected:''},
-      {name:'IFC Vendor',selected:''},
-      {name:'Vendor Capacity',selected:''},
-      {name:'Ranking',selected:''},
-      {name:'Deactivation date',selected:''},
-
-    ]
-
+  constructor(private fb: FormBuilder,
+    private changeDetector: ChangeDetectorRef) {
 
   }
 
   ngOnInit(): void {
-  }
-
-  checkBoxName(event:any,isChecked:boolean) {
     debugger
-    // if (isChecked) {
-    //   this.checked.push(event.target.value)
-    // }
-    // else {
-    //   let index = this.checked.indexOf(event.target.value);
-    //   this.checked.splice(index, 1);
-    // }
-    // this.filter = !this.filter;
-  }
- 
-  rightData():void{
-    debugger
-    this.checked.push(this.options);
-    // this.options = this.checked.splice(7);
-     this.rightArray = this.checked[0];
-    this.options = [];
+    this.options = [
+      { id: 1, name: 'Min Cost Of Printing', selected: false },
+      { id: 2, name: 'Min Shipping Time', selected: false },
+      { id: 3, name: 'Vendor Performance', selected: false },
+      { id: 4, name: 'IFC Vendor', selected: false },
+      { id: 5, name: 'Vendor Capacity', selected: false },
+      { id: 6, name: 'Ranking', selected: false },
+      { id: 7, name: 'Deactivation date', selected: false },
+
+    ];
+
+    this.leftCheckArray = JSON.parse(JSON.stringify(this.options));
+    this.changeDetector.detectChanges();
   }
 
-  leftData(){
-    debugger
-    this.rightcheck.push(this.rightArray);
-    // this.rightArray = this.rightcheck.splice(7);
-    this.options = this.rightcheck[0];
-    this.rightArray = [];
-
-    // this.options.push(this.check);
+  checkBoxName(event: any, isChecked: boolean) {
   }
 
-  movedata(){
-     debugger
-     this.selected = true;
-     
+  rightSidePush(): void {
+    this.rightCheckArray = JSON.parse(JSON.stringify(this.leftCheckArray));
+    this.leftCheckArray = [];
+    this.changeDetector.detectChanges();
+  }
+
+  leftSidePush(): void {
+    this.leftCheckArray = JSON.parse(JSON.stringify(this.rightCheckArray));
+    this.rightCheckArray = [];
+    this.changeDetector.detectChanges();
+  }
+
+  movedataLeft(event: MatCheckboxChange) {
+    let val = this.leftCheckArray.find(x => x.id == parseInt(event.source.name));
+    this.tempCheckArray.push(val);
+  }
+
+  movedataRight(event: MatCheckboxChange) {
+    let val = this.rightCheckArray.find(x => x.id == parseInt(event.source.name));
+    this.tempCheckArray.push(val);
 
   }
 
-  right(){
-    debugger
-  this.selected = true;
-  this.checked.push(this.selected);
-  this.rightArray = this.checked;
+  right() {
+    this.rightCheckArray = JSON.parse(JSON.stringify(this.tempCheckArray));
+    for (let index = 0; index < this.tempCheckArray.length; index++) {
+      const element = this.tempCheckArray[index];
+      this.leftCheckArray = this.leftCheckArray.filter(x => x.id !== element.id);
+    }
+    this.tempCheckArray = [];
 
+
+
+  }
+
+  left() {
+    this.leftCheckArray = JSON.parse(JSON.stringify(this.tempCheckArray));
+    for (let index = 0; index < this.tempCheckArray.length; index++) {
+      const element = this.tempCheckArray[index];
+      this.rightCheckArray = this.rightCheckArray.filter(x => x.id !== element.id);
+    }
+    this.tempCheckArray = [];
+    // this.rightCheckArray = [];
+  }
 
 }
 
-  left(){
-
-  }
-
+export class EnumValue {
+  id: number;
+  name: string;
+  selected: boolean;
 }
